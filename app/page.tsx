7d16241f61abdc92d9b17e4821960ca11c1d90c1
VillageR88 <villager88pl@gmail.com>
2024-06-23 11:57:29 +0200

    initial background day
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,15 @@ import imageBackgroundDaytime from '@/public/assets/desktop/bg-image-daytime.jpg
 export default function Home() {
   return (
     <>
-      <Image priority className="object-contain" fill src={imageBackgroundDaytime} alt="background image daytime" />
+      <Image
+        className="absolute left-0 size-full object-cover"
+        height={800}
+        priority
+        width={1440}
+        src={imageBackgroundDaytime}
+        alt="background image daytime"
+      />
+      <main></main>
     </>
   );
 }
